9bbed0cca0ea239710744208a23cca4f4db0efc6
Дмитрий <sh.diman@gmail.com>
2023-11-21 23:20:56 +0300

    добавить страницу поиска

--- presentation/Store.Web/Program.cs
+++ presentation/Store.Web/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Store;
+using Store.Memory;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IBookRepository, BookRepository>();
 
 var app = builder.Build();
 
